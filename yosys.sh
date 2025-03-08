cd92cf5a92bf8b8d9fb0e3968927d4513737b248
ICSteve <icsteve@x3p6ldtm.vm>
2025-03-08 09:39:54 +0800

    Add source code download in yosys install script

--- yosys.sh
+++
@@ -1,9 +0,0 @@
-mkdir ~/.local/repo/yosys ;
-cd ~/.local/repo/yosys ;
-wget https://github.com/YosysHQ/oss-cad-suite-build/releases/download/2025-03-07/oss-cad-suite-linux-x64-20250307.tgz
-tar xzf oss-cad-suite-linux-x64-20250307.tgz
-rm -fr oss-cad-suite-linux-x64-20250307.tgz ;
-setenv PATH $HOME/.local/repo/yosys/oss-cad-suite/bin
-
-yosys --version
-
